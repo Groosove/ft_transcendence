88f5ae0abe9c7487563fc92591072b3752c88da7
Groosove <arturianec2013@gmail.com>
2021-03-15 11:11:52 +0300

    install rails and npm and add some description

--- hello.rb
+++ hello.rb
@@ -1,26 +1,60 @@
 puts "Hello World!' + 'My name is Artur"
+# $a - глобальная переменная
 
-def print_word(name = 'Anonymous')
+def print_word(name: "Anonymous")
   puts "Hello, #{name.capitalize}!"
 end
 
-print_word('FLAVON')
+print_word(name: "Flavon")
+print_word()
 
 # Greeter
 class Greeter
+  attr_accessor :name
   def initialize(name = 'Anonymous')
     @name = name
   end
 
   def hello
-    p "Hi, #{@name}!"
+  if @name.nil?
+    p "Hi, Anonymous!"
+    elsif @name.respond_to?("each")
+      @name.each do |name|
+        p "Hi, #{name}"
+      end
+    end
   end
 
   def goodbye
-    puts "Bye, #{@name}!"
+    if @name.nil?
+      puts "Bye, Anonymous!"
+    elsif @name.respond_to?("each") 
+      @name.each do |name|
+        p "Bye, #{name}!"
+      end
+    end
   end
+
 end
 
 greeter = Greeter.new('Flavon')
 greeter.hello
 greeter.goodbye
+
+greeter.name = ["Artur","Sjakku", "Fkathryn"]
+greeter.hello
+greeter.goodbye
+
+x = 10
+y = 11
+X = 15
+#X = 20 С большой буквы пишутся константы
+
+z =  x < y ? true : false
+puts z
+z = if x > y
+      true 
+    else 
+      false
+    end
+puts z
