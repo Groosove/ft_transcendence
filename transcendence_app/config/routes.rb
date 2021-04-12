e95746307465213dc3a679d36b1fccb7347afe99
Groosove <arturianec2013@gmail.com>
2021-04-12 20:35:54 +0300

    redirect add

--- transcendence_app/config/routes.rb
+++ transcendence_app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "users#mainPage"
+  root "users#index"
 
+  get '/users/mainPage', as: 'user_root'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
