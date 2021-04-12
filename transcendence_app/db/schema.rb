f1a16db1ee878184e194fc413e3931db699d58d1
Groosove <arturianec2013@gmail.com>
2021-04-12 19:54:09 +0300

    add devise to transe

--- transcendence_app/db/schema.rb
+++ transcendence_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_232354) do
+ActiveRecord::Schema.define(version: 2021_04_12_161131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,16 +53,17 @@ ActiveRecord::Schema.define(version: 2021_04_10_232354) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_nickname"
-    t.string "user_emal"
-    t.string "user_pass"
-    t.string "user_role"
-    t.string "user_photo"
-    t.integer "user_rating"
-    t.string "intra_token"
-    t.integer "google_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname"
+    t.string "lol"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
