a2447b84370f2af1cd690ac76daa718955e28828
Groosove <arturianec2013@gmail.com>
2021-04-12 20:38:47 +0300

    add loginPage in user views

--- transcendence_app/config/routes.rb
+++ transcendence_app/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#index"
 
-  get '/users/mainPage', as: 'user_root'
+  get '/users/loginPage', as: 'user_root'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
